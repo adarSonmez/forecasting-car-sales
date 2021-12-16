679545d0488d1a838dffd0436c95cedd16d766fa
Adar Sönmez <adarsonmez@outlook.com>
2021-12-16 12:43:15 +0300

    empty forecast classes created for yusuf

---
+++ src/main/java/com/tesla/forecast/DoubleExponentialSmoothing.java
@@ -0,0 +1,19 @@
+package com.tesla.forecast;
+
+import com.tesla.models.Dataset;
+
+public class DoubleExponentialSmoothing {
+    private static double MSE;
+
+    public static void initializeForecast(int numOfRecordsForEachMonth, Dataset dataset) {
+
+    }
+
+    public static double getMSE() {
+        return MSE;
+    }
+
+    private static void calcMSE() {
+        // to do
+    }
+}
---
+++ src/main/java/com/tesla/forecast/RegressionAnalysis.java
@@ -0,0 +1,19 @@
+package com.tesla.forecast;
+
+import com.tesla.models.Dataset;
+
+public class RegressionAnalysis {
+    private static double MSE;
+
+    public static void initializeForecast(int numOfRecordsForEachMonth, Dataset dataset) {
+
+    }
+
+    public static double getMSE() {
+        return MSE;
+    }
+
+    private static void calcMSE() {
+        // to do
+    }
+}
