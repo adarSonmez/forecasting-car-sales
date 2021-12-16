a2246a56f71158cbc7f6f31cbe40b361ef653edd
Adar Sönmez <adarsonmez@outlook.com>
2021-12-16 12:41:01 +0300

    MyDoublyLinkedList data structure implemented

---
+++ src/main/java/com/tesla/ds/MyDoublyLinkedList.java
@@ -0,0 +1,228 @@
+package com.tesla.ds;
+
+public class MyDoublyLinkedList<T> implements Iterable<T>, MyList<T> {
+    private int size = 0;
+    private Node<T> head = null;
+    private Node<T> tail = null;
+
+    public void clear() {
+        Node<T> temp = head;
+        while (temp != null) {
+            Node<T> next = temp.next;
+            temp.prev = temp.next = null;
+            temp.data = null;
+            temp = next;
+        }
+        head = tail = null;
+        size = 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    @Override
+    public void add(T elem) {
+        if (isEmpty()) {
+            head = tail = new Node<T>(elem, null, null);
+        } else {
+            tail.next = new Node<T>(elem, tail, null);
+            tail = tail.next;
+        }
+        size++;
+    }
+
+    private void addFirst(T elem) {
+        if (isEmpty()) {
+            head = tail = new Node<T>(elem, null, null);
+        } else {
+            head.prev = new Node<T>(elem, null, head);
+            head = head.prev;
+        }
+        size++;
+    }
+
+    public void add(int index, T data) throws Exception {
+        if (index < 0 || index > size) {
+            throw new Exception("Illegal Index");
+        }
+        if (index == 0) {
+            addFirst(data);
+            return;
+        }
+
+        if (index == size) {
+            add(data);
+            return;
+        }
+
+        Node<T> temp = head;
+        for (int i = 0; i < index - 1; i++) {
+            temp = temp.next;
+        }
+        Node<T> newNode = new Node<>(data, temp, temp.next);
+        temp.next.prev = newNode;
+        temp.next = newNode;
+
+        size++;
+    }
+
+    @Override
+    public T first() {
+        if (isEmpty()) throw new RuntimeException("Empty list");
+        return head.data;
+    }
+
+    @Override
+    public T last() {
+        if (isEmpty()) throw new RuntimeException("Empty list");
+        return tail.data;
+    }
+
+    @Override
+    public T removeFirst() {
+        if (isEmpty()) throw new RuntimeException("Empty list");
+
+        T data = head.data;
+        head = head.next;
+        --size;
+
+        if (isEmpty()) tail = null;
+
+        else head.prev = null;
+        return data;
+    }
+
+    @Override
+    public T removeLast() {
+        if (isEmpty()) throw new RuntimeException("Empty list");
+
+        T data = tail.data;
+        tail = tail.prev;
+        --size;
+
+        if (isEmpty()) head = null;
+        else tail.next = null;
+        return data;
+    }
+
+    private T remove(Node<T> node) {
+        if (node.prev == null) return removeFirst();
+        if (node.next == null) return removeLast();
+
+        node.next.prev = node.prev;
+        node.prev.next = node.next;
+
+        T data = node.data;
+
+        node.data = null;
+        node = node.prev = node.next = null;
+
+        --size;
+        return data;
+    }
+
+    @Override
+    public T removeAt(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException();
+        }
+
+        int i;
+        Node<T> temp;
+
+        if (index < size / 2) {
+            for (i = 0, temp = head; i != index; i++) {
+                temp = temp.next;
+            }
+        } else {
+            for (i = size - 1, temp = tail; i != index; i--) {
+                temp = temp.prev;
+            }
+        }
+        return remove(temp);
+    }
+
+    @Override
+    public int indexOf(Object obj) {
+        int index = 0;
+        Node<T> temp = head;
+
+        if (obj == null) {
+            for (; temp != null; temp = temp.next, index++) {
+                if (temp.data == null) {
+                    return index;
+                }
+            }
+        } else {
+            for (; temp != null; temp = temp.next, index++) {
+                if (obj.equals(temp.data)) {
+                    return index;
+                }
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean contains(Object obj) {
+        return indexOf(obj) != -1;
+    }
+
+    @Override
+    public java.util.Iterator<T> iterator() {
+        return new java.util.Iterator<T>() {
+            private Node<T> temp = head;
+
+            @Override
+            public boolean hasNext() {
+                return temp != null;
+            }
+
+            @Override
+            public T next() {
+                T data = temp.data;
+                temp = temp.next;
+                return data;
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[ ");
+        Node<T> temp = head;
+        while (temp != null) {
+            sb.append(temp.data);
+            if (temp.next != null) {
+                sb.append(", ");
+            }
+            temp = temp.next;
+        }
+        sb.append(" ]");
+        return sb.toString();
+    }
+
+    private static class Node<T> {
+        private T data;
+        private Node<T> prev, next;
+
+        public Node(T data, Node<T> prev, Node<T> next) {
+            this.data = data;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+}
