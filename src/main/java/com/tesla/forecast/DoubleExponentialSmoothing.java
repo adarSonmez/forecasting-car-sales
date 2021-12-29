f8992e5be5b79d89c5c2eabd1daea51c344f1913
Adar Sönmez <adarsonmez@outlook.com>
2021-12-29 12:37:44 +0300

    double exponential fixed

--- src/main/java/com/tesla/forecast/DoubleExponentialSmoothing.java
+++ src/main/java/com/tesla/forecast/DoubleExponentialSmoothing.java
@@ -4,10 +4,10 @@ import com.tesla.ds.MyArrayList;
 import com.tesla.models.Dataset;
 
 public class DoubleExponentialSmoothing {
-    private static final double DE_ALPHA = 0.2;
+    private static final double ALPHA = 0.2;
     private static final double BETA = 0.2;
-    private static final int INITIAL_S = 200;
-    private static final int INITIAL_G = 50;
+    private static final double INITIAL_S = 200;
+    private static final double INITIAL_G = 50;
     private static double MSE;
 
     private static int maxForecastedSales;
@@ -21,23 +21,24 @@ public class DoubleExponentialSmoothing {
         MyArrayList<Integer> arr = dataset.getListOfRecords();
         MyArrayList<Integer> frr = new MyArrayList<>();
 
-        MyArrayList<Integer> arrS = new MyArrayList<>();
+        MyArrayList<Double> arrS = new MyArrayList<>();
         arrS.add(INITIAL_S);
-        MyArrayList<Integer> arrG = new MyArrayList<>();
+        MyArrayList<Double> arrG = new MyArrayList<>();
         arrG.add(INITIAL_G);
 
         for (int i = 1; i < arr.size() + 1; i++) {
-            double result1 = (DE_ALPHA * arr.get(i - 1) + (1 - DE_ALPHA) * (arrS.get(i - 1) + arrG.get(i - 1)));
-            arrS.add((int) result1);
+            double result1 = (ALPHA * arr.get(i - 1) + (1 - ALPHA) * (arrS.get(i - 1) + arrG.get(i - 1)));
+            arrS.add(result1);
 
             double result2 = (BETA * (arrS.get(i) - arrS.get(i - 1)) + (1 - BETA) * (arrG.get(i - 1)));
-            arrG.add((int) result2);
+            arrG.add(result2);
         }
 
         for (int i = 1; i < arr.size() + 1; i++) {
-            double forcast = arrS.get(i) + arrG.get(i);
-            frr.add((int) forcast);
+            double forecast = arrS.get(i) + arrG.get(i);
+            frr.add((int) forecast);
         }
+
         MSE = ForecastUtil.calcMSE(dataset, frr);
         minForecastedSales = ForecastUtil.getMinForecastedSales(frr);
         maxForecastedSales = ForecastUtil.getMaxForecastedSales(frr);
